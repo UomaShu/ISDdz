132cb716c5002982cb5ce9b83c230fb8458de952
UomaShu <rozariokun@gmail.com>
2018-04-20 19:54:16 +0300

    Blabla second

---
+++ abstract/Task_1/Task_1/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int a;
+            XMLHandler xmlHandler = new XMLHandler();
+            TXTHandler txtHandler = new TXTHandler();
+            DOCHandler docHandler = new DOCHandler();
+            Console.WriteLine("Выберите тип файла:1-TXT,2-DOC,3-XML");
+            while(flag)
+            {
+                a = int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        int x;
+                        Console.WriteLine("Что с ним нужно сделать:1-Открыть,2-Создать,3-Изменить,4-Сохранить");
+                        x = int.Parse(Console.ReadLine());
+                        switch (x)
+                        {
+                            case 1:
+                                txtHandler.Open();
+                                break;
+                            case 2:
+                                txtHandler.Create();
+                                break;
+                            case 3:
+                                txtHandler.Chenge();
+                                break;
+                            case 4:
+                                txtHandler.Save();
+                                break;
+                            default:
+                                Console.WriteLine("Error!");
+                                break;
+                        }
+                        break;
+                    case 2:
+                        int y;
+                        Console.WriteLine("Что с ним нужно сделать:1-Открыть,2-Создать,3-Изменить,4-Сохранить");
+                        y = int.Parse(Console.ReadLine());
+                        switch (y)
+                        {
+                            case 1:
+                                docHandler.Open();
+                                break;
+                            case 2:
+                                docHandler.Create();
+                                break;
+                            case 3:
+                                docHandler.Chenge();
+                                break;
+                            case 4:
+                                docHandler.Save();
+                                break;
+                            default:
+                                Console.WriteLine("Error!");
+                                break;
+                        }
+                        break;
+                    case 3:
+                        int i;
+                        Console.WriteLine("Что с ним нужно сделать:1-Открыть,2-Создать,3-Изменить,4-Сохранить");
+                        i = int.Parse(Console.ReadLine());
+                        switch (i)
+                        {
+                            case 1:
+                                xmlHandler.Open();
+                                break;
+                            case 2:
+                                xmlHandler.Create();
+                                break;
+                            case 3:
+                                xmlHandler.Chenge();
+                                break;
+                            case 4:
+                                xmlHandler.Save();
+                                break;
+                            default:
+                                Console.WriteLine("Error!");
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Error!");
+                        break;
+                }
+            }
+
+        }
+    }
+
+    abstract class AbstractHandler
+    {
+        abstract public void Open();
+        abstract public void Create();
+        abstract public void Chenge();
+        abstract public void Save();
+    }
+
+    class XMLHandler: AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Файл разширения XML открыт");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("Файл разширения XML создан.");
+        }
+        public override void Chenge()
+        {
+            Console.WriteLine("Файл разширения XML изменен.");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Файл разширения XML сохранен.");
+        }
+    }
+     class TXTHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Файл разширения TXT открыт");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("Файл разширения TXT создан.");
+        }
+        public override void Chenge()
+        {
+            Console.WriteLine("Файл разширения TXT изменен.");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Файл разширения TXT сохранен.");
+        }
+    }
+     class DOCHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Файл разширения DOC открыт");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("Файл разширения DOC создан.");
+        }
+        public override void Chenge()
+        {
+            Console.WriteLine("Файл разширения DOC изменен.");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Файл разширения DOC сохранен.");
+        }
+    }
+
+}
---
+++ abstract/Task_2/Task_2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    interface IPlayable
+    {
+        void Play();
+        void Pause(); 
+        void Stop();
+    }
+    interface IRecordable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+    class Player:IPlayable,IRecordable
+    {
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Проигрывание поставлено на паузу.");
+        }
+        void IRecordable.Pause()
+        {
+            Console.WriteLine("Запись поставлена на паузу.");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Проигрывание остановлено.");
+        }
+        void IRecordable.Stop()
+        {
+            Console.WriteLine("Запись остановлена.");
+        }
+        public void Play()
+        {
+            Console.WriteLine("Проигрывание начато.");
+        }
+        public void Record()
+        {
+            Console.WriteLine("Запись начата.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int choice;
+            Player player = new Player();
+            IPlayable playableMethods = player;
+            IRecordable recordableMethods = player;
+            Console.WriteLine("Добро пожаловать в плеер! Выберите, что вы хотите сделать:");
+            Console.WriteLine("1.Начать проигрывание");
+            Console.WriteLine("2.Поставить проигрывание на паузу");
+            Console.WriteLine("3.Остановить проигрыывание");
+            Console.WriteLine("4.Начать запись");
+            Console.WriteLine("5.Поставить запись на паузу");
+            Console.WriteLine("6.Остановить запись");
+            Console.WriteLine("7.Выключить");
+            while (flag)
+            {
+
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        player.Play();
+                        Console.WriteLine("Выберете следующие действие:");
+                        break;
+                    case 2:                        
+                        playableMethods.Pause();
+                        Console.WriteLine("Выберете следующие действие:");
+                        break;
+                    case 3:                       
+                        playableMethods.Stop();
+                        Console.WriteLine("Выберете следующие действие:");
+                        break;
+                    case 4:
+                        player.Record();
+                        Console.WriteLine("Выберете следующие действие:");
+                        break;
+                    case 5:
+                        recordableMethods.Pause();
+                        Console.WriteLine("Выберете следующие действие:");
+                        break;
+                    case 6:
+                        recordableMethods.Stop();
+                        Console.WriteLine("Выберете следующие действие:");
+                        break;
+                    case 7:
+                        Console.WriteLine("Выключение");
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неверный ввод!");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
