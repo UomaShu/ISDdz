272499fba2194d9df07bd762630fcea677467e67
Arseniy <arseniy.myak@gmail.com>
2018-04-27 22:09:32 +0300

    Myakenkyi Arseniy

---
+++ Struct.Task_4-6/Task_4/Task_4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day, month, year, nextYear;
+            Console.WriteLine("Введите день рождения:");
+            day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ведите число месяца рождения:");
+            month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ведите год рождения:");
+            year = int.Parse(Console.ReadLine());
+            DateTime dateOfBirth = new DateTime(year,month,day);
+            DateTime todayDate = DateTime.Today;
+            Console.WriteLine("Вы родились {0}", dateOfBirth.ToShortDateString());
+            DateTime dateNext;
+            if (dateOfBirth.Month.CompareTo(todayDate.Month)<0 || (dateOfBirth.Month.CompareTo(todayDate.Month)==0 && dateOfBirth.Day.CompareTo(todayDate.Day)<0))
+            {
+                nextYear = Convert.ToInt32(todayDate.Year)+1;
+                dateNext = new DateTime(nextYear, month, day);
+                Console.WriteLine("Дней до следующего дня рождения:{0}", (dateNext.Subtract(todayDate)).TotalDays);
+            }
+            else if (dateOfBirth.Month.CompareTo(todayDate.Month) > 0 || (dateOfBirth.Month.CompareTo(todayDate.Month) == 0 && dateOfBirth.Day.CompareTo(todayDate.Day) >= 0))
+            {
+                nextYear = Convert.ToInt32(todayDate.Year);
+                dateNext = new DateTime(nextYear, month, day);
+                Console.WriteLine("Дней до следующего дня рождения:{0}", (dateNext.Subtract(todayDate)).TotalDays);
+            }      
+            //DateTime dateNext = new DateTime(Convert.ToInt32(DateTime.Today.Year),month, day);
+            //Console.WriteLine("Дней до следующего дня рождения:{0}",(dateNext.Subtract(todayDate)).TotalDays);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Struct.Task_4-6/Task_5/Task_5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    enum Colors
+    {
+        Green,
+        Red,
+        Blue,
+        Cyan,
+        Magenta
+    }
+    static class StaticClass
+    {
+        public static void Print(string stroka, int color)
+        {
+            switch (color)
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(stroka);
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(stroka);
+                    break;
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(stroka);
+                    break;
+                case 4:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(stroka);
+                    break;
+                case 5:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(stroka);
+                    break;
+                default:
+                    break;
+            }
+
+        } 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int choice;
+            Console.WriteLine("Программа позволяет выводить заданную строку выбранным цветом.");
+            Console.WriteLine("Доступные цвета:");
+            string[] colorNames = Colors.GetNames(typeof(Colors));
+            for (int i=0; i<colorNames.Length; i++)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, colorNames[i]);
+            }
+            Console.WriteLine("Введите строку:");
+            str = Console.ReadLine();
+            Console.WriteLine("Ведиет номер цвета:");
+            choice = int.Parse(Console.ReadLine());
+            StaticClass.Print(str, choice);           
+            Console.ReadKey();
+        }
+    }
+}
