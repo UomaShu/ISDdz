e91cdab0f702565cb36aa1f3ee9d75ffb106ed59
SoftRaven <arseniy.myak@gmail.com>
2018-04-28 22:03:25 +0300

    Myakenkyi Arseniy

---
+++ Delegate.Tasks_2-3/Task_2/Task_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        public delegate double Operations(double x, double y);
+        static void Main(string[] args)
+        {
+            string sign;
+            double x, y, result;
+            Operations Add = (a, b) => a + b;
+            Operations Sub = (a, b) => a - b;
+            Operations Mul = (a, b) => a * b;
+            Operations Div = (a, b) => b==0 && a<0 ? double.NegativeInfinity : b==0 ? double.PositiveInfinity:a/b;
+            Console.WriteLine("Ввведите первое число:");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ведите ввторое число:");
+            y = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите действие: +, -, *,/ ");
+            sign = Console.ReadLine();
+            switch (sign)
+            {
+                case "+":
+                    result = Add(x, y);
+                    Console.WriteLine("Ответ:{0}",result);                    
+                    break;
+                case "-":
+                    result = Sub(x, y);
+                    Console.WriteLine("Ответ:{0}", result);
+                    break;
+                case "*":
+                    result = Mul(x, y);
+                    Console.WriteLine("Ответ:{0}", result);
+                    break;
+                case "/":
+                    result = Div(x, y);
+                        Console.WriteLine("Ответ:{0}", result);
+                    break;
+                default:
+                    Console.WriteLine("Неверный ввод");
+                    break;                               
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Delegate.Tasks_2-3/Task_3/Task_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public delegate int Number();
+    public delegate int Medium(Number[] array);  
+    class Program
+    {
+        static Random rand = new Random();
+        public static int Randomizer()
+        {
+            return rand.Next(1, 10);
+        }
+        static void Main(string[] args)
+        {
+            Number[] array = new Number[5];
+            for (int i=0; i<array.Length; i++)
+            {
+                array[i] = Randomizer;
+            }
+            Console.WriteLine("Сгенерированные числа:");
+            Medium mediumValue = delegate (Number[] numArray)
+             {
+                 int sum = 0;
+                 int arrayElem;
+                 for (int i = 0; i < numArray.Length; i++)
+                 {
+                     arrayElem = numArray[i]();
+                     Console.WriteLine(arrayElem);
+                     sum += arrayElem;
+                 }
+                 return ((sum) / (numArray.Length));
+             };
+            Console.WriteLine("Среднее арифметическое:{0}", mediumValue(array));
+            Console.ReadKey();
+        }
+    }
+}
