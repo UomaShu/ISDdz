b41ca02c9cc421b6eec1ff32195c1328eec7bb2b
UomaShu <rozariokun@gmail.com>
2018-04-28 12:55:10 +0300

    Klimov

---
+++ Delegate/delegate_task_1/delegate_task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_task_1
+{
+    class Program
+    {
+        delegate int Operation(int x, int y,int n);
+        static void Main(string[] args)
+        {
+            Operation operation = delegate (int x, int y,int n)
+            {
+                return (x + y + n) / 3;
+            };
+            int d = operation(4, 5, 6);
+            Console.WriteLine(d); 
+            Console.Read();
+        }
+    }
+}
