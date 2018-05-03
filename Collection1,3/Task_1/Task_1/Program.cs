30d1852f3273fcaa3e126513dd66374632e6a427
UomaShu <rozariokun@gmail.com>
2018-05-04 00:11:17 +0300

    Klimov

---
+++ Collection1,3/Task_1/Task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 5, 4, 2, 8, 4, 10, 12 };
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.Write("\n");
+            Console.WriteLine("Коллекция четных элементов:");
+            foreach (var item in MyClass.MyMeth(array))
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.Write("\n");
+            Console.ReadKey();
+
+        }
+    }
+
+       static class  MyClass
+    {
+        public  static IEnumerable<int> MyMeth(int[] array)
+        {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+                yield return array[i];
+        }
+        }
+    }
+}
+ 
+
---
+++ Collection1,3/Tusk-3/Tusk-3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tusk_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(0, "string");
+            dictionary.Add(1, "string1");
+            dictionary.Add(2, "string2");
+            dictionary.Add(3, "string3");
+            dictionary.Add(4, "string4");
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+    class MyDictionary<Tkey, Tval>: IEnumerable<KeyValuePair<Tkey,Tval>>
+    {
+        private int counter = 0;
+        private Tkey[] keysArray = null;
+        private Tval[] valsArray = null;
+        
+        public int Counter
+        {
+            get { return this.counter; }
+        }
+
+        public void Add(Tkey key, Tval val)
+        {
+            this.counter++;
+
+            Array.Resize(ref keysArray, counter);
+            keysArray[counter - 1] = key;
+
+            Array.Resize(ref valsArray, counter);
+            valsArray[counter - 1] = val;
+        }
+
+        public Tval this[Tkey key]
+        {
+            get
+            {
+                int ind = 0;
+                for (int i = 0; i < keysArray.Length; i++)
+                {
+                    if (key.Equals(keysArray[i]))
+                        ind = i;
+                }
+                return valsArray[ind];
+            }
+        }
+        public IEnumerator<KeyValuePair<Tkey, Tval>> GetEnumerator()
+        {
+            for (int i = 0; i < keysArray.Length; i++)
+                yield return new KeyValuePair<Tkey, Tval>(keysArray[i], valsArray[i]);
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
