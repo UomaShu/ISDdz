3f96182bff97379e703c6b3073c21e555d91bad5
UomaShu <rozariokun@gmail.com>
2018-05-05 15:20:19 +0300

    Klimov

---
+++ Input-Output_1/Task-1/Task-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Test
+{
+    public static void Main()
+    {
+        
+        string path = @"E:\\ISD\\Folder_";
+
+            for (var i = 0; i < 100; i++)
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path+i);
+                Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
+
+              
+                di.Delete();
+                Console.WriteLine("The directory was deleted successfully.");
+            }
+       
+
+
+    }
+}
